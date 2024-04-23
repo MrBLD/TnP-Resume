1449ef84603f1e7cb0ab14ea50434ab1d1aeae6a
Bhaskar Lal Das <120587599+MrBLD@users.noreply.github.com>
2024-04-23 21:54:42 +0530

    navbar fix

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import './NavBar.css'
 
 function Navbar() {
     return (
-        <div className=' flex w-full border-b-2 border-slate-300 fixed z-10 '>
+        <div className=' flex w-full border-b-2 fixed z-10 '>
             {/* <div className=' flex w-full dark:bg-white/[0.5] bg-black/[0.5] border-b-2 border-slate-300'> */}
             <div className='font-semibold text-3xl m-4 '>TnP@NITAP</div>
             <div className='flex justify-center flex-end end-1 absolute mt-4 mr-5 text-xl'>
