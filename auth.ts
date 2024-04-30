712823f87bc2261ab15842de511ef1fa3a0a2ba7
Bhaskar Lal Das <120587599+MrBLD@users.noreply.github.com>
2024-04-30 23:11:34 +0530

    auth

---
+++ auth.ts
@@ -0,0 +1,71 @@
+import NextAuth from "next-auth";
+
+import Apple from "next-auth/providers/apple";
+import Auth0 from "next-auth/providers/auth0";
+import AzureB2C from "next-auth/providers/azure-ad-b2c";
+import BoxyHQSAML from "next-auth/providers/boxyhq-saml";
+import Cognito from "next-auth/providers/cognito";
+import Coinbase from "next-auth/providers/coinbase";
+import Discord from "next-auth/providers/discord";
+import Dropbox from "next-auth/providers/dropbox";
+import Facebook from "next-auth/providers/facebook";
+import GitHub from "next-auth/providers/github";
+import Gitlab from "next-auth/providers/gitlab";
+import Google from "next-auth/providers/google";
+import Hubspot from "next-auth/providers/hubspot";
+import Keycloak from "next-auth/providers/keycloak";
+import LinkedIn from "next-auth/providers/linkedin";
+import Netlify from "next-auth/providers/netlify";
+import Okta from "next-auth/providers/okta";
+import Passage from "next-auth/providers/passage";
+import Pinterest from "next-auth/providers/pinterest";
+import Reddit from "next-auth/providers/reddit";
+import Slack from "next-auth/providers/slack";
+import Spotify from "next-auth/providers/spotify";
+import Twitch from "next-auth/providers/twitch";
+import Twitter from "next-auth/providers/twitter";
+import WorkOS from "next-auth/providers/workos";
+import Zoom from "next-auth/providers/zoom";
+
+import type { NextAuthConfig } from 'next-auth';
+
+import facebook from "next-auth/providers/facebook";
+import linkedin from "next-auth/providers/linkedin";
+import github from "next-auth/providers/github";
+import discord from "next-auth/providers/discord";
+import twitch from "next-auth/providers/twitch";
+import twitter from "next-auth/providers/twitter";
+import hubspot from "next-auth/providers/hubspot";
+
+export const config = {
+  theme: { logo: "https://authjs.dev/img/logo-sm.png" },
+  providers: [
+    Google,
+    facebook,
+    linkedin,
+    github,
+    discord,
+    twitch,
+    twitter,
+    Apple,
+    AzureB2C,
+    WorkOS({
+      connection: process.env.AUTH_WORKOS_CONNECTION!,
+    }),
+    Zoom,
+  ],
+  basePath: "/auth",
+  callbacks: {
+    authorized({ request, auth }) {
+      const { pathname } = request.nextUrl;
+      if (pathname === "/middleware-example") return !!auth;
+      return true;
+    },
+    jwt({ token, trigger, session }) {
+      if (trigger === "update") token.name = session.user.name;
+      return token;
+    },
+  },
+} satisfies NextAuthConfig;
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
